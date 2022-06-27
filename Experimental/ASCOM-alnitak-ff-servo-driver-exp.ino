c9aa860ececbf3cb3f8d8d76743d1a98b4101825
AEVE <vargas.espinosa.angel@gmail.com>
2022-06-27 16:57:39 -0500

    Ni idea.

---
+++ Experimental/ASCOM-alnitak-ff-servo-driver-exp.ino
@@ -0,0 +1,24 @@
+//Código Experimental. No usar en producción.
+
+
+#include <Servo.h>
+
+Servo servoA;
+
+int pos = 0
+
+void setup(){
+    servoA.attach(9);
+}
+
+void loop() {
+    for (pos = 0; pos <= 180; pos +=1)
+
+    myservo.write(pos);
+    delay(15);
+
+    for (pos = 180; pos >= 0; pos -= 1){
+         myservo.write(pos);
+        delay(15);
+    }
+}
