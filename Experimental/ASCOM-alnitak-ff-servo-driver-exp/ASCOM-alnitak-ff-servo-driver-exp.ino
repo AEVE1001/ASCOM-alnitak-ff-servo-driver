3dddc50c207a3f2e8e0c2ef79b472456ca1d5683
AEVE <vargas.espinosa.angel@gmail.com>
2022-06-27 16:57:29 -0500

    Corrección de nombres de variable

--- Experimental/ASCOM-alnitak-ff-servo-driver-exp/ASCOM-alnitak-ff-servo-driver-exp.ino
+++ Experimental/ASCOM-alnitak-ff-servo-driver-exp/ASCOM-alnitak-ff-servo-driver-exp.ino
@@ -5,7 +5,7 @@
 
 Servo servoA;
 
-int pos = 0
+int pos = 0;
 
 void setup(){
     servoA.attach(9);
@@ -14,11 +14,11 @@ void setup(){
 void loop() {
     for (pos = 0; pos <= 180; pos +=1)
 
-    myservo.write(pos);
+    servoA.write(pos);
     delay(15);
 
     for (pos = 180; pos >= 0; pos -= 1){
-         myservo.write(pos);
+         servoA.write(pos);
         delay(15); 
     }
 }
